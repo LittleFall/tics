affe53a9bd26173ff33d42acb9ca195ec00535a4
Zhi Qi <1578166061@qq.com>
2021-01-21 16:21:01 +0800

    revert 2

--- cmake/find_ssl.cmake
+++ cmake/find_ssl.cmake
@@ -5,7 +5,7 @@ if (NOT USE_INTERNAL_SSL_LIBRARY)
         set (OPENSSL_ROOT_DIR "/usr/local/opt/openssl")
         # https://rt.openssl.org/Ticket/Display.html?user=guest&pass=guest&id=2232
         if (USE_STATIC_LIBRARIES)
-            message (STATUS "Disable USE_STATIC_LIBRARIES if you have linking problems with OpenSSL on MacOS")
+            message (WARNING "Disable USE_STATIC_LIBRARIES if you have linking problems with OpenSSL on MacOS")
         endif ()
     endif ()
     find_package (OpenSSL)
