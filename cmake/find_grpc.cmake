4b05ab466ccbd79b228bf25f2378668730f7d19d
Zhi Qi <1578166061@qq.com>
2021-01-21 13:54:25 +0800

    pause commit

--- cmake/find_grpc.cmake
+++ cmake/find_grpc.cmake
@@ -1,19 +1,40 @@
-# TODO We should make a bundled gPRC and protoobuf repository, instance of rely on system library.
+option (USE_INTERNAL_GRPC_LIBRARY "Set to FALSE to use system grpc library instead of bundled" ${NOT_UNBUNDLED})
 
-# gRPC and relateds
+# gRPC and relates
 
-find_package(Protobuf REQUIRED)
-message(STATUS "Using protobuf: ${Protobuf_VERSION} : ${Protobuf_INCLUDE_DIRS}, ${Protobuf_LIBRARIES}")
+if (USE_INTERNAL_GRPC_LIBRARY AND NOT EXISTS "${ClickHouse_SOURCE_DIR}/contrib/grpc/CMakeLists.txt")
+    message (WARNING "submodules in contrib/grpc is missing. to fix try run: \n git submodule update --init --recursive")
+    set (USE_INTERNAL_GRPC_LIBRARY 0)
+endif()
 
-include_directories(${PROTOBUF_INCLUDE_DIRS})
+if (USE_INTERNAL_GRPC_LIBRARY)
+    add_subdirectory(contrib/grpc ${CMAKE_CURRENT_BINARY_DIR}/grpc EXCLUDE_FROM_ALL)
+    message(STATUS "Using gRPC via add_subdirectory.")
 
-find_package(c-ares REQUIRED)
-message(STATUS "Lib c-ares found")
+    # After using add_subdirectory, we can now use the grpc targets directly from
+    # this build.
+    set(_PROTOBUF_LIBPROTOBUF libprotobuf)
+    set(_PROTOBUF_PROTOC $<TARGET_FILE:protoc>)
+    set(_GRPC_GRPCPP_UNSECURE grpc++_unsecure)
+    set(_GRPC_CPP_PLUGIN_EXECUTABLE $<TARGET_FILE:grpc_cpp_plugin>)
+else()
+    message(WARNING "${USE_INTERNAL_GRPC_LIBRARY}")
 
-find_package(ZLIB REQUIRED)
-message(STATUS "Using ZLIB: ${ZLIB_INCLUDE_DIRS}, ${ZLIB_LIBRARIES}")
+    message(WARNING "${USE_INTERNAL_GRPC_LIBRARY}")
+    find_package(Protobuf REQUIRED)
+    message(STATUS "Using protobuf: ${Protobuf_VERSION} : ${Protobuf_INCLUDE_DIRS}, ${Protobuf_LIBRARIES}")
 
-find_package(gRPC CONFIG REQUIRED)
-message(STATUS "Using gRPC: ${gRPC_VERSION}")
+    include_directories(${PROTOBUF_INCLUDE_DIRS})
 
-set(gRPC_FOUND TRUE)
+    find_package(c-ares REQUIRED)
+    message(STATUS "Lib c-ares found")
+
+    find_package(ZLIB REQUIRED)
+    message(STATUS "Using ZLIB: ${ZLIB_INCLUDE_DIRS}, ${ZLIB_LIBRARIES}")
+
+    find_package(gRPC CONFIG REQUIRED)
+    message(STATUS "Using gRPC: ${gRPC_VERSION}")
+
+    set(gRPC_FOUND TRUE)
+endif()
+
